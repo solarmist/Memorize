921fb2cd5d5d1e14b7ab05e8d95a288d33abd26f
Joshua Olson <joshua.olson+github@gmail.com>
2020-11-09 20:48:09 -0800

    Initial Commit

---
+++ Shared/MemorizeApp.swift
@@ -0,0 +1,20 @@
+//
+//  MemorizeApp.swift
+//  Shared
+//
+//  Created by Joshua Olson on 11/9/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
